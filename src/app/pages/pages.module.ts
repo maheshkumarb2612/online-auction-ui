065f2c7a671ec194f12221fbca55d60903c3a7af
Yogeshkumar <yogeshkumarb57@gmail.com>
2019-12-29 10:04:13 +0530

    chanes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './public/register/register.component';
 import { ProfileComponent } from './secured/profile/profile/profile.component';
 import { PostProductComponent } from './secured/post-product/post-product.component';
 import {MatLoaderModule} from '../mat-loader.module';
+import { ProductDetailComponent } from './public/product-detail/product-detail.component';
 
 
 const routes: Routes = [
@@ -14,12 +15,13 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'post-product', component: PostProductComponent }
+  { path: 'post-product', component: PostProductComponent },
+  { path: 'product-detail', component: ProductDetailComponent}
 ]
 
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent, RegisterComponent, ProfileComponent, PostProductComponent],
+  declarations: [HomeComponent, LoginComponent, RegisterComponent, ProfileComponent, PostProductComponent, ProductDetailComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
